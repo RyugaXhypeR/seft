68981841fb82e4b28d1d38de15cf02c2c90cbe39
RyugaXhypeR <ryugashyper@gmail.com>
2023-08-08 19:23:56 +0530

    Addition: implemented a dynamic array to store directories.

---
+++ include/attr_list.h
@@ -0,0 +1,28 @@
+#ifndef ATTR_LIST_H
+#define ATTR_LIST_H
+
+#include <stdbool.h>
+
+#include <libssh/sftp.h>
+
+typedef struct {
+    /** Array of directories pushed to the list
+     *
+     * .. note::
+     *
+     *    The type of the attribute should always be ``SSH_FILEXFER_TYPE_DIRECTORY`` */
+    sftp_attributes *dirs;
+
+    /** Length of the array */
+    size_t length;
+
+    /** Size allocated for the array */
+    size_t allocated;
+} AttrListT;
+
+AttrListT *AttrList_new(size_t length);
+void AttrList_push(AttrListT *self, sftp_attributes attr);
+bool AttrList_is_empty(AttrListT *self);
+void AttrList_free(AttrListT *self);
+
+#endif
