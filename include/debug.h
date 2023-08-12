2cbbe89ed42a9a94c7991ca467d3494c1d88b079
RyugaXhypeR <ryugashyper@gmail.com>
2023-08-12 23:36:11 +0530

    Change: removed underscore prefix.

--- include/debug.h
+++ include/debug.h
@@ -17,23 +17,23 @@ enum DBG_LEVELS {
     DBG_LEVEL_CRITICAL,
 };
 
-#define LOG(level, prompt, ...)                                                       \
-    do {                                                                              \
-        if (DBG_STATUS) {                                                             \
-            fprintf(stderr, "[%s]:%s:%d:%s: " prompt "\n", __dbg_level_to_str(level), \
-                    __FILE__, __LINE__, __func__, __VA_ARGS__);                       \
-        }                                                                             \
-        if (level == DBG_LEVEL_CRITICAL) {                                            \
-            fprintf(stderr, prompt "\n", __VA_ARGS__);                                \
-        }                                                                             \
+#define LOG(level, prompt, ...)                                                     \
+    do {                                                                            \
+        if (DBG_STATUS) {                                                           \
+            fprintf(stderr, "[%s]:%s:%d:%s: " prompt "\n", dbg_level_to_str(level), \
+                    __FILE__, __LINE__, __func__, __VA_ARGS__);                     \
+        }                                                                           \
+        if (level == DBG_LEVEL_CRITICAL) {                                          \
+            fprintf(stderr, prompt "\n", __VA_ARGS__);                              \
+        }                                                                           \
     } while (0)
 
 #define DBG_ERR(prompt, ...) LOG(DBG_LEVEL_CRITICAL, prompt, __VA_ARGS__)
 #define DBG_INFO(prompt, ...) LOG(DBG_LEVEL_INFO, prompt, __VA_ARGS__)
 #define DBG_DEBUG(prompt, ...) LOG(DBG_LEVEL_DEBUG, prompt, __VA_ARGS__)
 
-inline char*
-__dbg_level_to_str(enum DBG_LEVELS level) {
+static inline char*
+dbg_level_to_str(enum DBG_LEVELS level) {
     switch (level) {
         case DBG_LEVEL_DEBUG:
             return "DEBUG";
