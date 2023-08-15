ad262ea5d8d3305fe58fc10477291166827a3dbd
RyugaXhypeR <ryugashyper@gmail.com>
2023-08-15 12:17:50 +0530

    Addition: added new methods

--- include/sftp_list.h
+++ include/sftp_list.h
@@ -22,10 +22,12 @@ typedef struct {
 
 ListT *List_new(size_t length, size_t type_size);
 void List_push(ListT *self, void *other, size_t size);
+void List_re_alloc(ListT *self, size_t new_size);
 ListT *List_slice(ListT *self, size_t start, size_t stop);
 void *List_pop(ListT *self);
 void *List_get(ListT *self, size_t index);
 bool List_is_empty(ListT *self);
 void List_free(ListT *self);
+void List_copy_inplace(ListT *self, ListT *dest, size_t size);
 
 #endif /* SFTP_LIST_H */
