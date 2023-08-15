89039db56c0254e5858776c02506be44aec41dc2
RyugaXhypeR <ryugashyper@gmail.com>
2023-08-15 17:46:56 +0530

    Addition: added mew methods to check flags for `list` command

--- include/sftp_utils.h
+++ include/sftp_utils.h
@@ -13,8 +13,10 @@
 
 uint32_t get_window_column_length(void);
 uint32_t u32_list_sum(ListT *self);
-uint32_t char_list_max_len(ListT *self);
+size_t char_list_max_len(ListT *self);
 ListT *char_list_equalized_slice(ListT *self, size_t len_rows, size_t len_cols);
 void char_list_format_columnwise(ListT *self, size_t width_screen, char *delimiter);
+bool check_show_hidden(char *path_str, size_t length, uint8_t flag);
+bool check_path_type(char *path_str, size_t length, bool is_dir, uint8_t flag);
 
 #endif /* ifndef SFTP_UTILS_H */
