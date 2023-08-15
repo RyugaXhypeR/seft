d7d0dfc1ba74b6e4b72ed110f599e751b7bb0dad
RyugaXhypeR <ryugashyper@gmail.com>
2023-08-15 12:19:07 +0530

    Addition: new macros

--- include/sftp_utils.h
+++ include/sftp_utils.h
@@ -2,10 +2,19 @@
 #define SFTP_UTILS_H
 
 #include <stdint.h>
+#include "sftp_list.h"
 
-#define BIT_MATCH(bits, pos) (bits & (1 << pos))
-#define BIT_SET(bits, mask) (bits |= (1 << pos))
+#define BIT_MATCH(bit_mask, pos) ((bit_mask) & (1 << (pos)))
+#define BIT_SET(bit_mask, pos) ((bit_mask) |= (1 << (pos)))
+#define CEIL(dividend, divisor) ((dividend) / (divisor) + (dividend) % (divisor))
+
+#define BUF_SIZE_FORMATTED_COLUMNWISE 16384
+#define BUF_SIZE_LINE_LIMIT 1024
 
 uint32_t get_window_column_length(void);
+uint32_t u32_list_sum(ListT *self);
+uint32_t char_list_max_len(ListT *self);
+ListT *char_list_equalized_slice(ListT *self, size_t len_rows, size_t len_cols);
+void char_list_format_columnwise(ListT *self, size_t width_screen, char *delimiter);
 
 #endif /* ifndef SFTP_UTILS_H */
