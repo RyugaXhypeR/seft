6750b265d9f5a740e73d663d7f66982cb31e6754
RyugaXhypeR <ryugashyper@gmail.com>
2023-08-16 19:25:12 +0530

    Addition: simple `config.h` to customize colors and icons of the program

---
+++ src/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "sftp_ansi_colors.h"
+
+#define ICON_FOLDER ""
+#define ICON_GIT_FOLDER ""
+#define ICON_FILE ""
+
+#define COLOR_FOLDER ANSI_FG_BLUE
+#define COLOR_FILE ANSI_FG_GREEN
+
+#endif /* CONFIG_H */
