7aaa7a8f1e95f12b273681b016a0f65934af75ea
RyugaXhypeR <ryugashyper@gmail.com>
2023-08-19 17:07:37 +0530

    Change: removed command handlers and `ArgT`

--- include/commands.h
+++ include/commands.h
@@ -3,12 +3,6 @@
 
 #include <stddef.h>
 
-#define MAX_COMMAND_NAME_LENGTH 64
-#define MAX_COMMAND_HELP_MSG_LENGTH 1024
-#define MAX_NUM_COMMANDS 6
-
-#define COMMAND(name) cli_command_##name
-
 /** A enum to represent the status of a command */
 typedef enum {
     /** Command executed successfully */
@@ -29,46 +23,5 @@ typedef enum {
     /** Command not executed */
     CMD_NOT_EXECUTED = -1
 } CommandStatusE;
-
-/** A structure to hold the arguments passed to a command */
-typedef struct {
-    /** Arguments passed to subcommands of the program. */
-    char **args;
-
-    /* Number of arguments passed to subcommands of the program. */
-    size_t num_args;
-} ArgsT;
-
-/** A structure to hold information about a command */
-typedef struct {
-    /** Name of the command */
-    char command_name[MAX_COMMAND_NAME_LENGTH + 1];
-
-    /** Help message for the command */
-    char command_help_msg[MAX_COMMAND_HELP_MSG_LENGTH + 1];
-
-    /** Function pointer to the command handler */
-    CommandStatusE (*command_handler)(ArgsT args);
-
-    /** Status of the command */
-    CommandStatusE command_status;
-} CommandHandlerT;
-
-CommandHandlerT *CommandHandler_new(const char *command_name,
-                                    const char *command_help_msg,
-                                    CommandStatusE (*command_handler)(ArgsT args));
-void CommandHandler_free(CommandHandlerT *self);
-void CommandHandler_display_help_msg(CommandHandlerT *self);
-
-/** Helper function */
-CommandHandlerT *get_command_handler_from_name(const char *command_name);
-
-/** Commands */
-CommandStatusE COMMAND(help)(ArgsT args);
-CommandStatusE COMMAND(list)(ArgsT args);
-CommandStatusE COMMAND(create)(ArgsT args);
-CommandStatusE COMMAND(remove)(ArgsT args);
-CommandStatusE COMMAND(copy)(ArgsT args);
-CommandStatusE COMMAND(move)(ArgsT args);
 
 #endif /* COMMANDS_H */
